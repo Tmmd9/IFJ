9adf2a7f68b8f602fb89430df052864820448c53
Dávid Oravec <xorave05@vutbr.cz>
2019-10-19 16:08:52 +0200

    commiting error header

---
+++ error.h
@@ -0,0 +1,28 @@
+/*
+ *	Predmet : Formální jazyky a pøekladaèe
+ *	Projekt : Implementace pøekladaèe imperativního jazyka IFJ19
+ *	Súbor	: error.h
+ *				-hlavièkový súbor pre error kódy
+ *	Tím		: èíslo 10 / varianta II
+ *	Autori	: Dominik Boboš (xbobos00)
+ *			  Peter Hudeèek (xhudec34)
+ *			  Timotej Kováèik (xkovac49)
+ *			  Dávid Oravec (xorave05)
+ *
+ */
+
+#ifndef _ERROR_H_
+#define _ERROR_H_
+
+#define TOKEN_OK 0
+#define SYNTAX_OK 0
+#define ERROR_SCANNER 1					//chyba v lex.analyze
+#define ERROR_PARSER 2					//chyba v syntakt. analyze
+#define ERROR_PROGRAM_SEMANTIC 3		//chyba semantiky (nedefinovana var,.)
+#define ERROR_ARTIHMETIC 4				//semanticka aritmeticka chyba
+#define ERROR_WRONG_NUMBER_OF_PARAMS 5	//semanticka chyba(zly pocet param.)
+#define ERROR_SEMANTIC_OTHERS 6			//ine semanticke chyby
+#define ERROR_DIVIDING_ZERO 9			//delenie nulou
+#define ERROR_INTERN 99					//interna chyba programu (alokacia)
+
+#endif // !_ERROR_H_
