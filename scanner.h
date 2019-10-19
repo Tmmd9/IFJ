fa199c23b2d89c335d73ec05678fc03f4cac2d15
Dávid Oravec <xorave05@vutbr.cz>
2019-10-19 17:10:00 +0200

    finished enum

--- scanner.h
+++ scanner.h
@@ -16,35 +16,44 @@
 
 
 typedef enum {
-	STATE_START,				// startovaci stav
-	STATE_ERROR = 1,			// lexikalna chyba
-	STATE_ID,					// konecny stav identifikatoru
-	STATE_KEYWORD,				// konecny stav
-	STATE_INT,					// konecny stav intu
-	STATE_FLOAT,				// konecny stav FLOAT
-	STATE_FLOAT_POINT,			// medzistav float + desatinna ciarka
-	STATE_FLOAT_POINT_NUMBER,	// medzistav float + des. ciarka + cislo
-	STATE_FLOAT_EXP,			// medzistav float a exponent
-	STATE_FLOAT_EXP_OP,			// medzistav napr. 2^-e or 2^+e
-	STATE_STRING,				// konecny stav
-	STATE_START,				// medzistav, zacina string
-	STATE_BACKSLASH,			// medzistav 
-	STATE_BACKSLASH_NUMBER,		// medzistav na hexakod
-	STATE_PLUS,					// konecny stav operatoru +
-	STATE_MINUS,				// konecny stav operatoru -
-	STATE_MULTIPLY,				// konecny stav operatoru *
-	STATE_DIVIDE,				// konecny stav operatoru /
-	STATE_DIVIDE_INT,
-	STATE_GREATER_THAN,
-	STATE_LESS_THAN,
-	STATE_GREATER_EQUAL,
-	STATE_LESS_EQUAL,
-	STATE_EQUAL,
-	STATE_NOT_EQUAL,
-	STATE_COMMENT,
-	STATE_LEFT_PAR,
-	STATE_RIGHT_PAR,
-	//TODO
+	STATE_START,					// startovaci stav {S}
+	STATE_ERROR = -1,				// lexikalna chyba
+	STATE_ID_OR_KEYWORD,			// konecny stav {F20}
+	STATE_INT,						// konecny stav intu {F1}
+	STATE_FLOAT,					// konecny stav {F3}
+	STATE_FLOAT_POINT,				// medzistav {Q1}
+	STATE_FLOAT_POINT_NUMBER,		// konecny stav {F2}
+	STATE_FLOAT_EXP,				// medzisstav {Q2}
+	STATE_FLOAT_EXP_OP,				// medzistav {Q3}
+	STATE_STRING,					// konecny stav {F21}
+	STATE_STRING_START,				// medzistav {Q6}
+	STATE_BACKSLASH,				// medzistav {Q7}
+	STATE_BACKSLASH_X,				// medzistav {Q8}
+	STATE_BACKSLASH_X_NUM,			// medzistav {Q9}		
+	STATE_PLUS,						// konecny stav {F5}
+	STATE_MINUS,					// konecny stav {F4}
+	STATE_MULTIPLY,					// konecny stav {F6}
+	STATE_DIVIDE,					// konecny stav {F7}
+	STATE_DIVIDE_INT,				// konecny stav {F8}
+	STATE_GREATER_THAN,				// konecny stav {F11}
+	STATE_LESS_THAN,				// konecny stav {F9}
+	STATE_GREATER_EQUAL,			// konecny stav {F12}
+	STATE_LESS_EQUAL,				// konecny stav {F10}
+	STATE_EQUALS,					// konecny stav {F17}
+	STATE_NOT_EQUAL_START,			// medzistav {Q4}
+	STATE_NOT_EQUAL,				// konecny stav {F13}
+	STATE_EQUAL_TO,					// konecny stav {F18}
+	STATE_SEMICOLON,				// konecny stav {F14}
+	STATE_COMMENT,					// medzistav {Q5}
+	STATE_LEFT_PAR,					// konecny stav {F15}
+	STATE_RIGHT_PAR,				// konecny stav {F16}
+	STATE_COMMA,					// konecny stav {F19}
+	STATE_BLOCK_COMMENT_START,		// medzistav {Q10}
+	STATE_BLOCK_COMMENT_CONTINUE,	// medzistav {Q11}
+	STATE_BLOCK_COMMENT,			// medzistav {Q12}
+	STATE_BLOCK_COMMENT_LEAVE_TRY,	// medzistav {Q13}
+	STATE_BLOCK_COMMENT_LEAVE,		// medzistav {Q14}
+	STATE_EOL,						// konecny stav {F22}
 } state;
 
 typedef struct {
@@ -53,4 +62,4 @@ typedef struct {
 
 token getNextToken();
 
-#endif
+#endif	// !_SCANNER_H_
