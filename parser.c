cfa96e515cff6ef0eca511e466b31402bcf83805
Dominik Boboš <bobos.dominik@gmail.com>
2019-11-09 19:17:45 +0100

    Fix in LL_table, v parseri zacate pravidlo 1 a 14, podoplnane veci do ParserData

--- parser.c
+++ parser.c
@@ -19,26 +19,97 @@
 #include <ctype.h>
 #include "expr.h"
 
+FILE *source;
+
+void sourceFile(FILE *f) {
+    source = f;
+}
 
 /*
- *
- *
- * soemthing
+ *  Funkcia na porovnavanie očakávaného token.typu
+ *  s tym co nam realne prijde so suboru vo forme dalsieho
+ *  tokenu
  */
-
-#define GET_TOKEN()													\
-	if ((result = get_next_token(&data->token)) != SCANNER_TOKEN_OK)\
-		return result
+int checkTokenType(FILE *source, token *token, tStack *stack, token_type type)
+{
+    int returncode;
+    if((returncode = getNextToken(f, &token, data->stack)) == TOKEN_OK)
+    {
+        if (type == token.type)
+            return SYNTAX_OK;
+        else
+            return ERROR_PARSER;
+    }
+    return returncode;
+}
 
 
 static int prog(ParserData* data)
 {
     int result;
-    //<prog> -> KEYWORD_DEF TYPE_INENTIFIER(<params>): TYPE_EOL TYPE_INDENT <statement> TYPE_EOL TYPE_DEDENT <prog>
-    if (data->token.attribute.keyword == KEYWORD_DEF && data->token.type == TYPE_KEYWORD)
-    {
+//1. <prog> -> KEYWORD_DEF TYPE_INENTIFIER(<params>): TYPE_EOL TYPE_INDENT <statement> TYPE_EOL TYPE_DEDENT <prog>
+    if (data->token.attribute.keyword == KEYWORD_DEF && data->token.type == TYPE_KEYWORD) {
+        data->in_declaration = 1;           //je true
+        data->not_declared_function = 1;    //je true
 
+        // "Identifikátor funkcie"
+        if ((result = checkTokenType(f, &data->Token, data->stack, TYPE_IDENTIFIER)) == 0) {
+            
+            //pridam identifier do tabulky symbolov// checknem ci nenastava redefinicia
+            
+            if (/*insert searchFName(nieco, token->attribute.string)*/ != 0)
+                return ERROR_PROGRAM_SEMANTIC;   ///pokial sa najde zhoda, je tu pokus o redefiniciu
+            else
+                //pridam identifier funkcie do tabulky;
+
+            // "("
+            if ((result = checkTokenType(f, &data->Token, data->stack, TYPE_LEFT_PAR)) == 0) {
+                data->paramIndex = 0;
+                //preskocim na params (poslem im "data" ktore uchovavaju info
+                //ci sme v deklaracii alebo mimo nej atd..
+                if ((result = params(&data)) == 0) {
+                    if (data->Token.type == TYPE_RIGHT_PAR) {  //toto je hlavne test, ptom sa toto ifko moze odstranit
+                        //todo-> : TYPE_EOL TYPE_INDENT <statement> TYPE_EOL TYPE_DEDENT <prog>
+                    }
+                    else ///ked sa nieco posralo a neprisla mi z params(&data) ')'
+                        return ERROR_PARSER;    
+                }
+            }
+            else
+                return result;
+        }
+        else
+            return result;
     }
+
+
 }
 
+static int params(ParserData *data)
+{
+    static int result;
+    if (data->in_declaration == 1) {
+        if ((result = checkTokenType(f, &data->Token, data->stack, TYPE_IDENTIFIER)) == 0) {
+            data->paramIndex += 1;
 
+            //skontrolujem ci uz zadana funkcia nema rovnako nazvany parameter
+
+            //nacitavam dalsi token ak je ciarka ocakavam dalsi param.
+            //14. <param_next> -> TYPE_COMMA TYPE_IDENTIFIER <param_next>
+            if ((result = checkTokenType(f, &data->Token, data->stack, TYPE_COMMA)) == 0)
+                params(&data);
+            else if (data->Token == TYPE_RIGHT_PAR) {
+                //ulozim ze dana funkcia ma zatial N paramaterov podla data->paramIndex
+                return SYNTAX_OK
+            }
+            else ERROR_PARSER;
+        }   
+            //nacitany token je prava zatvorka -> <params> -> ε
+        else if (data->Token == TYPE_RIGHT_PAR && data->paramIndex == 0) {
+            //ulozim ze dana funcia nepotrebuje parametre
+            return SYNTAX_OK;
+        }
+        else
+            return ERROR_PARSER; ///param index bol vacsi ako nula ale dosla mi hned zatvorka
+    }                            ///pripadne za ciarkou neprisiel dalsi identif
+}
--- parser.h
+++ parser.h
@@ -6,11 +6,26 @@
 
 //struktura co bude uchovavat vsetko dolezite pre parser
 typedef struct {
+    symTable globalT;               //na tabulku globalnych symbolov (identifikatorov etc)
+    symTable localT;                //na tabulku localnych symbolov (identifikatorov etc)
 	/*Neni dokoncene, ked ta napadne co
 	sem pridat tak to sem pridaj a daj koment ze preco */
 	token Token;
-	/*sem pojdu ukazatele na predchadzajuci,nasledujuci 
+    tStack *stack;                  //dunno if necessary ale tak kvoli INDENTOM pre scanner
+
+	/*sem pojdu ukazatele na predchadzajuci,nasledujuci
+	 * smting like TDATA* currentID
+	 *              TDATA * lefthandside_var
+	 *              TDATA * righthandside_var
 	a sucastny symbol v hash tabulke */
+
+    // ked checkujem parametre, tak index toho ktory momentalne spracovavam
+	unsigned paramIndex;
+	//int uniqLabel;
+	//int deepLabel;
+
+    unsigned in_declaration;        //bool ci sa momentalne nachadzam v deklaracii funkcie
+    unsigned not_declared_function; //bool ci sa nachadzam este v nedeklarovanej funkci
 
 } ParserData;
 
