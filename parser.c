fd29f4cec0ba6d94e1efeef0d05472308550b281
PeterHudecek <48943615+PeterHudecek@users.noreply.github.com>
2019-11-14 01:20:58 +0100

    https://youtu.be/G6j5Z2Hm0AM?t=115
    Link je reakcia každého čo sa pozre čo sa pičovinu som to stvoril, niečo ako keď Dr Mengele skúšal pokusy na deťoch

--- parser.c
+++ parser.c
@@ -85,6 +85,155 @@ static int prog(ParserData* data)
 
 }
 
+static int statement(ParserData *data)
+{   
+    int result;
+    // 5. <statement> -> KEYWORD_IF <expression> TYPE_COLON TYPE_EOL TYPE_INDENT <statement> TYPE_EOL
+    // TYPE_DEDENT KEYWORD_ELSE TYPE_COLON TYPE_EOL TYPE_INDENT <statement> TYPE_EOL 
+    // TYPE_DEDENT <statement_next>
+
+    //IF
+    if ((result = checkTokenType(f, &data->Token, data->stack, TYPE_KEYWORD))
+     && data->token.attribute.keyword == KEYWORD_IF) {
+
+        /***TODO EXPRESSION***/ //neviem ci to bude funkcia abo co
+
+        //COLON a.k.a dvojbodka
+        if((result = checkTokenType(f, &data->Token, data->stack, TYPE_COLON)) == 0) {
+            //EOL
+            if((result = checkTokenType(f, &data->Token, data->stack, TYPE_EOL)) == 0) {
+                //INDENT
+                if((result = checkTokenType(f, &data->Token, data->stack, TYPE_INDENT)) == 0) {
+                    //rekurzia pre vnutro IF-u
+                    statement(&data);
+                    /**** nejsom si isty ci treba kontrolovat eol tu ked to kontroluje kazdy
+                    statement, vyzaduje midlife crisis na pochopenie ****/
+                    if((result = checkTokenType(f, &data->Token, data->stack, TYPE_EOL)) == 0) {
+                        //DEDENT
+                        if((result = checkTokenType(f, &data->Token, data->stack, TYPE_DEDENT)) == 0) {
+                            //ELSE
+                            if ((result = checkTokenType(f, &data->Token, data->stack, TYPE_KEYWORD))
+                            && data->token.attribute.keyword == KEYWORD_ELSE) {
+                                //COLON a.k.a dvojbodka
+                                if((result = checkTokenType(f, &data->Token, data->stack, TYPE_COLON)) == 0) {
+                                    //EOL
+                                    if((result = checkTokenType(f, &data->Token, data->stack, TYPE_EOL)) == 0) {
+                                        //INDENT
+                                        if((result = checkTokenType(f, &data->Token, data->stack, TYPE_INDENT)) == 0) {
+                                            //rekurzia pre vnutro ELSE
+                                            statement(&data);
+                                            /**** nejsom si isty ci treba kontrolovat eol tu ked to kontroluje kazdy
+                                            statement, vyzaduje midlife crisis na pochopenie ****/
+                                            if((result = checkTokenType(f, &data->Token, data->stack, TYPE_EOL)) == 0) {
+                                                //DEDENT
+                                              if((result = checkTokenType(f, &data->Token, data->stack, TYPE_DEDENT)) == 0) {
+                                                //pokracovanie kodiku, koniec tohto LL pravidla
+                                                statement_next(&data);
+                                              } 
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } 
+            }
+        }
+        //SYNTAX ERR
+        else
+            return result;
+
+    }
+
+    // 6. <statement> -> KEYWORD_WHILE TYPE_COLON <expression> TYPE_COLON TYPE_EOL TYPE_INDENT <statement> TYPE_EOL TYPE_DEDENT <statement_next>
+    //WHILE
+    else if ((result = checkTokenType(f, &data->Token, data->stack, TYPE_KEYWORD))
+     && data->token.attribute.keyword == KEYWORD_WHILE) {
+        //COLON a.k.a dvojbodka
+        if((result = checkTokenType(f, &data->Token, data->stack, TYPE_COLON)) == 0) {
+
+            /**** TODO EXPRESSION ****/
+
+            //COLON a.k.a dvojbodka
+            if((result = checkTokenType(f, &data->Token, data->stack, TYPE_COLON)) == 0) {
+            //EOL
+                if((result = checkTokenType(f, &data->Token, data->stack, TYPE_EOL)) == 0) {
+                //INDENT
+                    if((result = checkTokenType(f, &data->Token, data->stack, TYPE_INDENT)) == 0) {
+                        //vnutro while-u rekurzia statementu
+                        statement(&data);
+                        //EOL
+                        if((result = checkTokenType(f, &data->Token, data->stack, TYPE_EOL)) == 0) {
+                            //DEDENT
+                            if((result = checkTokenType(f, &data->Token, data->stack, TYPE_DEDENT)) == 0) {
+                                //pokracovanie kodiku, koniec tohto LL pravidla
+                                statement_next(&data);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        //SYNTAX ERR 
+        else
+            return result;
+    }
+
+    // 7. <statement> -> KEYWORD_RETURN <expression>
+     else if ((result = checkTokenType(f, &data->Token, data->stack, TYPE_KEYWORD))
+     && data->token.attribute.keyword == KEYWORD_RETURN) {
+        /**** TODO EXPRESSION ****/
+        return result;
+    }
+
+    //8.  <statement> -> TYPE_IDENTIFIER TYPE_ASSIGN_VALUE <expression> TYPE_EOL <statement_next>
+    if ((result = checkTokenType(f, &data->Token, data->stack, TYPE_KEYWORD))
+     && data->token.attribute.keyword == KEYWORD_IF) {
+
+        /**** TODO hodenie do global/lokal symtable kedtak len prepis hodnoty ak to tam je****/
+        if ((result = checkTokenType(f, &data->Token, data->stack, TYPE_IDENTIFIER)) == 0) {
+            // =
+            if ((result = checkTokenType(f, &data->Token, data->stack, TYPE_IDENTIFIER)) == 0) {
+
+                /****TODO 2 EXPRESSION ****/
+
+                //EOL
+                if((result = checkTokenType(f, &data->Token, data->stack, TYPE_EOL)) == 0) {
+                    //pokracovanie kodiku, koniec tohto LL pravidla
+                    statement_next(&data);
+                }
+            }
+        }
+    }
+
+
+
+}
+
+//9.  <statement_next> -> <statement> || Epsilon (ukoncenie statement rekurzie) 
+
+/**** DIS IS BIG TODO IM RITORD NOW ****/  
+
+static int statement_next(ParserData *data){
+    /*ak bude prazdny riadok tak by som asi ukoncil rekurziu lebo neviem ako inak zistit 
+    ze je prazdny statement alebo ze zacal nejaky iny, celkovo som asi isto kokot
+    */
+    if((result = checkTokenType(f, &data->Token, data->stack, TYPE_EOL)) == 0) {
+        return;
+    }
+    else
+        statement(&data);
+}
+
+
+
+
+
+
+
+
+
 static int params(ParserData *data)
 {
     static int result;
@@ -113,4 +262,3 @@ static int params(ParserData *data)
             return ERROR_PARSER; ///param index bol vacsi ako nula ale dosla mi hned zatvorka
     }                            ///pripadne za ciarkou neprisiel dalsi identif
 }
-
